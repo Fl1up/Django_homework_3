f779503d2f67389cf10108c235fde1f85e1dc598
German <germanstarinenko@gmail.com>
2023-06-27 13:44:11 +0300

    исправление иконок

--- main/Product/templatetags/mediapath.py
+++ main/Product/templatetags/mediapath.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 register = template.Library()
 
+
 @register.simple_tag
 def mediapath(imag):
-    return settings.MEDIA_URL + imag
+    return f"{settings.MEDIA_URL}{imag}"
