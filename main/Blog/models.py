985ae18820e31bf580acb0ed6eabc415fcd1e366
German <germanstarinenko@gmail.com>
2023-07-04 10:48:08 +0300

    автозаполение блога

--- main/Blog/models.py
+++ main/Blog/models.py
@@ -6,9 +6,9 @@ ABLE = {"blank": False, "null": False}
 
 class Blog(models.Model):
     title = models.CharField(max_length=150, verbose_name="Название")
-    slug = models.CharField(max_length=150, verbose_name="Slug")
+    slug = models.CharField(max_length=150, verbose_name="Slug", **ABLE)
     body = models.TextField(verbose_name="Содержимое", **NULLABLE)
-    preview = models.ImageField(upload_to='media/', verbose_name="Превью", **NULLABLE)
+    preview = models.ImageField(upload_to='media/', verbose_name="Фото", **NULLABLE)
     create_data = models.DateField(default='2023-06-20', verbose_name="Дата создания", **NULLABLE)
     sign_publication = models.CharField(max_length=150, verbose_name="Признак публикации", **NULLABLE)
     count_views = models.IntegerField(default=0, verbose_name="Количество просмотров")
