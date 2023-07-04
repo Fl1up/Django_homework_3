439cede29033042fe9ee8ecbdfe5499450b031f7
German <germanstarinenko@gmail.com>
2023-07-04 10:44:04 +0300

    автозаполение блога

---
+++ main/Blog/management/commands/add_blog.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+
+from main.Blog.models import Blog
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        Blog.objects.all().delete(),
+
+
+        # fruits = Blog.objects.create(title='фрукты')
+        # vegetables = Blog.objects.create(title='овощи')
+        # berry = Blog.objects.create(title='ягоды')
+        # meet = Blog.objects.create(title='мясо')
+        # fish = Blog.objects.create(title='рыба')
+
+        Blog.objects.create(title='банан', body='', preview="/Банан.png")
+        Blog.objects.create(title='помидор', body='', preview='/Помидор.png')
+        Blog.objects.create(title='орех', body='',  preview='/Орех.png')
+        Blog.objects.create(title='баранина', body='', preview='/Баранина.png')
+        Blog.objects.create(title='курица', body='', preview='/Курица.png')
+        Blog.objects.create(title='килька', body='', preview='/Рыба.png')
+
+
+
+
+
+
