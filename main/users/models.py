4c1f988c6bc0c18969ca32c2f2e5b602c45082f3
German <germanstarinenko@gmail.com>
2023-07-13 13:13:59 +0300

    Готово, но поле пароля не убирается

---
+++ main/users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from main.Product.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="почта")
+
+    avatar = models.ImageField(upload_to="users/", verbose_name='аватар', **NULLABLE)
+    phone = models.CharField(max_length=35, verbose_name="телефон")
+    country = models.CharField(max_length=100, verbose_name='страна', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
